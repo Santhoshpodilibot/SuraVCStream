64b54abd0eeba7f6d9ec9fb529eab46b26070c62
Santhoshpodilibot <95113548+Santhoshpodilibot@users.noreply.github.com>
2022-02-07 19:16:56 +0530

    Update music.py

--- program/music.py
+++ program/music.py
@@ -139,7 +139,7 @@ async def play(c: Client, m: Message):
                 )
             else:
                 try:
-                    await suhu.edit("🔄 **Joining vc...**")
+                    await suhu.edit("😐 **ᴀʀᴇʏ ɴɪʙʙᴀ ᴠᴄ ᴋɪ ʙᴀᴛᴀɴᴀ ᴅʜᴀ...**")
                     await call_py.join_group_call(
                         chat_id,
                         AudioPiped(
@@ -167,7 +167,7 @@ async def play(c: Client, m: Message):
                     "» reply to an **audio file** or **give something to search.**"
                 )
             else:
-                suhu = await c.send_message(chat_id, "🔍 **Searching...**")
+                suhu = await c.send_message(chat_id, "🧐 **ɴɪʙʙᴀ ɢᴀ ᴀɢᴜ sᴇᴀʀᴄʜɪɴɢ ᴄʜᴇsᴛᴜɴᴀ**")
                 query = m.text.split(None, 1)[1]
                 search = ytsearch(query)
                 if search == 0:
@@ -200,7 +200,7 @@ async def play(c: Client, m: Message):
                             )
                         else:
                             try:
-                                await suhu.edit("🔄 **Joining vc...**")
+                                await suhu.edit("😐 **ᴀʀᴇʏ ɴɪʙʙᴀ ᴠᴄ ᴋɪ ʙᴀᴛᴀɴᴀ ᴅʜᴀ...**")
                                 await call_py.join_group_call(
                                     chat_id,
                                     AudioPiped(
@@ -230,7 +230,7 @@ async def play(c: Client, m: Message):
                 "» reply to an **audio file** or **give something to search.**"
             )
         else:
-            suhu = await c.send_message(chat_id, "🔍 **Searching...**")
+            suhu = await c.send_message(chat_id, "🧐 **ɴɪʙʙᴀ ɢᴀ ᴀɢᴜ sᴇᴀʀᴄʜɪɴɢ ᴄʜᴇsᴛᴜɴᴀ**")
             query = m.text.split(None, 1)[1]
             search = ytsearch(query)
             if search == 0:
@@ -261,7 +261,7 @@ async def play(c: Client, m: Message):
                         )
                     else:
                         try:
-                            await suhu.edit("🔄 **Joining vc...**")
+                            await suhu.edit("😐 **ᴀʀᴇʏ ɴɪʙʙᴀ ᴠᴄ ᴋɪ ʙᴀᴛᴀɴᴀ ᴅʜᴀ...**")
                             await call_py.join_group_call(
                                 chat_id,
                                 AudioPiped(
