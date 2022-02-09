26e3e5a3bbb5cd601c750ae0438ed567197a9fcb
Santhoshpodilibot <95113548+Santhoshpodilibot@users.noreply.github.com>
2022-02-09 22:19:27 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -55,7 +55,7 @@ async def _human_time_duration(seconds):
 )
 async def start_(client: Client, message: Message):
     await message.reply_text(
-        f""🧐 **Welcome {message.from_user.mention()} !**\n
+        f"""🧐 **Welcome {message.from_user.mention()} !**\n
 🤠 ʜᴇʏ\n ɪ'ᴍ *sᴀɴᴛʜᴏsʜ ᴍᴜsɪᴄ ʙᴏᴛ 😇*[!](https://te.legra.ph/file/38deca938d96e9d207b27.jpg)ʏᴏᴜʀ ᴀᴜᴅɪᴏ ᴀɴᴅ ᴠɪᴅᴇᴏ ᴍᴜsɪᴄ ʙᴏᴛ ɴᴀɴᴜ ᴍᴇ ɢʀᴏᴜᴘs ʟᴏ ᴀᴅᴅ ᴄʜᴇsᴜᴋᴏɴᴅɪ ᴘʟᴢ ʀᴀ ɴɪʙʙᴀ ɴᴇɴᴜ ʟᴀɢ ʟᴇᴋᴜɴᴅᴀ ᴍᴜsɪᴄ ᴘʟᴀʏ ᴄʜᴇsᴛʜᴀ 🥺** 
 """
         reply_markup=InlineKeyboardMarkup(
